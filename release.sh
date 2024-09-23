ff930234ae896ebaaab60d69623ac19feb28d5bc
Martijn van Kekem <martijn@vankekem.com>
2024-09-23 22:55:01 +0200

    Add release script

--- release.sh
+++ release.sh
@@ -3,5 +3,3 @@ rm -rf ./dist
 mkdir -p ./dist/better-refassist
 ./node_modules/typescript/bin/tsc -p ./src/js/tsconfig.json
 cp -rf ./popup.html ./manifest.json ./build ./assets ./dist/better-refassist
-cd ./dist || exit
-/usr/bin/zip better-refassist.zip -r ./*
