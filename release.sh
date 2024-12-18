f8a975bd204a02e9e4b4cec308c6c64c584e7c32
Martijn van Kekem <martijn@vankekem.com>
2024-12-18 08:31:56 +0100

    Fix release script

--- release.sh
+++ release.sh
@@ -3,3 +3,4 @@ rm -rf ./dist
 mkdir -p ./dist/better-refassist
 npm run build
 cp -rf ./popup.html ./manifest.json ./build ./assets ./dist/better-refassist
+zip -r ./dist/better-refassist.zip ./dist/better-refassist/*
