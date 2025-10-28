b50dde95fb4aea801c61a4933162a6eca661da05
Martijn van Kekem <martijn@vankekem.com>
2025-10-28 21:57:14 +0100

    Fix rules not included in package (#21)

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 rm -rf ./dist
 mkdir -p ./dist/better-refassist
 npm run build
-cp -rf ./popup.html ./manifest.json ./build ./assets ./dist/better-refassist
+cp -rf ./popup.html ./manifest.json ./include ./build ./assets ./dist/better-refassist
 zip -r ./dist/better-refassist.zip ./dist/better-refassist/*
 rm -rf ./dist/better-refassist
